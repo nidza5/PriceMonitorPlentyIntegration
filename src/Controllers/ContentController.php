72a0e56f37ce32270168c4dc42fd4613634f0c5a
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-11 10:59:15 +0200

    echo packagost result

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -54,7 +54,7 @@ namespace PriceMonitorPlentyIntegration\Controllers;
 				['email' => $credentials['email'], 'password' => $credentials['password']]
 			);
 
-            echo $libCall;
+            echo $packagistResult;
             // $proxy = Proxy::createFor($credentials['email'],$credentials['password']);        
              // $contracts = $packagistResult->getContracts();
 
