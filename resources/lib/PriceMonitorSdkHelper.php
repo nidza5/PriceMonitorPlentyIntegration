467348392399fe3d0f78a9e703aa37ce32654e46
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-11 15:24:21 +0200

    add http client

--- resources/lib/PriceMonitorSdkHelper.php
+++ resources/lib/PriceMonitorSdkHelper.php
@@ -1,11 +1,19 @@
 <?php
+ 
+ use Patagona\Pricemonitor\Core\Infrastructure\ServiceRegister;
  use Patagona\Pricemonitor\Core\Infrastructure\Proxy;
  use Patagona\Pricemonitor\Core\Infrastructure\Logger;
+ use PriceMonitorPlentyIntegration\PriceMonitorHttpClient;
 
  class PriceMonitorSdkHelper
  {
     public static function loginInPriceMonitor($email,$password)
     {
+        new ServiceRegister();
+
+        $client = new PriceMonitorHttpClient();
+        ServiceRegister::registerHttpClient($client);
+
         $proxy = Proxy::createFor($email,$password);      
         $contracts = $proxy->getContracts();
         return $contracts;
