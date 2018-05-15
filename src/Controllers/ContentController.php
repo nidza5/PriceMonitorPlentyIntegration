d124540ab2873235cffeac0e7c07f1ab7b75fd8c
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-15 15:38:20 +0200

    echo contract

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -74,6 +74,8 @@ namespace PriceMonitorPlentyIntegration\Controllers;
                 'password' => $credentials['password']
             ]);
 
+            echo json_encode($reponseContracts);
+
             if($reponseContracts != null && is_array($reponseContracts) && isset($reponseContracts['Code']) && isset($reponseContracts['Message']))
             {
                 $errorReponse = null;
