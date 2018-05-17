ec850b27d7e1747aea593cbdfe2560d903609325
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-17 13:20:39 +0200

    domain

---
+++ ui/index.php
@@ -0,0 +1,19 @@
+<?php
+
+use Plenty\Plugin\WebstoreConfiguration;
+
+
+$storeConfig = new WebstoreConfiguration();
+
+$domain = $storeConfig->domain;
+
+
+echo $domain;
+
+// <a href="https://dzonivasiljevic.plentymarkets-cloud01.com/loginPriceMonitor">PriceMonitor </a>
+
+?>
+
+
+
+
