d96495ca97a514a49476bbacadf8e199e8b5c7fd
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-21 17:45:01 +0200

    set tabs content

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,21 +19,6 @@ $(document).ready(function() {
 
     // Get the element with id="defaultOpen" and click on it
       document.getElementById("defaultOpen").click(); 
-
-    //   var acc = document.getElementsByClassName("accordion");
-    //   var i;
-      
-    //   for (i = 0; i < acc.length; i++) {
-    //     acc[i].addEventListener("click", function() {
-    //       this.classList.toggle("active");
-    //       var panel = this.nextElementSibling;
-    //       if (panel.style.maxHeight){
-    //         panel.style.maxHeight = null;
-    //       } else {
-    //         panel.style.maxHeight = panel.scrollHeight + "px";
-    //       } 
-    //     });
-    //   }
   });
 
 function expandCollapseMenu(el) {
@@ -46,5 +31,21 @@ function expandCollapseMenu(el) {
  var liExpand = $el.next('ul');
 
  liExpand.toggle();
+
+}
+
+function showTabContent(evt, tabName) {
+
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 
 }
