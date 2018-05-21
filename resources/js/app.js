089c49d10fa97db86edddd8c3c5bb967da077cca
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-21 11:02:59 +0200

    add colapse content js and css

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,5 +19,20 @@ $(document).ready(function() {
 
     // Get the element with id="defaultOpen" and click on it
       document.getElementById("defaultOpen").click(); 
-});
+
+      var acc = document.getElementsByClassName("accordion");
+      var i;
+      
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          } 
+        });
+      }
+  });
 
