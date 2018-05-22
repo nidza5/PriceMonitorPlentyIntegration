ba6b892c1b5e94c1d8eb37b77772aaeba2155d2f
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-22 15:04:56 +0200

    handle null exceptions in showTabContent

--- resources/js/app.js
+++ resources/js/app.js
@@ -50,7 +50,12 @@ function showTabContent(evt, tabName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
 
+    if(evt != null)
+    {
+        if(evt.currentTarget != null)
+            evt.currentTarget.className += " active";
+    }
 }
