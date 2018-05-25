13de2c4002229c29f62fddd8b62498d09211c8c8
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-25 11:31:14 +0200

    json stringify salesPriceImportIn

--- resources/js/app.js
+++ resources/js/app.js
@@ -106,7 +106,7 @@ function showTabContent(evt, tabName) {
         var data = {
             'id' : 0,
             'priceMonitorId': priceMonitorId,
-            'salesPriceImportInId': salesPriceImportIn,
+            'salesPriceImportInId': JSON.stringify(salesPriceImportIn),
             'isInsertSalesPrice' : isInsertSalesPrice
         };
 
