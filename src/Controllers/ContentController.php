b156042042a8bd714262b03e05011de9781f82f4
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-28 11:43:24 +0200

    change column names

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -134,7 +134,7 @@ namespace PriceMonitorPlentyIntegration\Controllers;
             'password' => $credentials['password']
         ]);
  
-            $columnsForStores = array("storeIdentifier","name");
+            $columnsForStores = array("id","name");
             $stores = $webStoreRepositoryContract->loadAll($columnsForStores);
 
             echo json_encode($stores);
