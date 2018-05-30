9ec2c7e52a496054a35637e5cb44ebcef24724d6
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-30 15:48:13 +0200

    access array value productFilter

--- resources/lib/FilterStorage.php
+++ resources/lib/FilterStorage.php
@@ -13,7 +13,7 @@ class FilterStorage implements FilterStorageInterface
     */
     private $productFilter;
 
-    public function __construct( ProductFilterRepository $productFilter)
+    public function __construct($productFilter)
     {
         $this->productFilter = $productFilter;
     }
@@ -42,7 +42,7 @@ class FilterStorage implements FilterStorageInterface
       */
      public function getFilter($contractId, $type)
      {
-         return ($this->productFilter !== null) ? $this->productFilter->serializedFilter : null;
+         return ($this->productFilter !== null) ? $this->productFilter['serializedFilter'] : null;
      }
 }
 
