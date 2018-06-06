493235b0404681871afddd091e037bd0b9b7612d
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-06 12:08:27 +0200

    without get populated filter

--- resources/lib/PriceMonitorSdkHelper.php
+++ resources/lib/PriceMonitorSdkHelper.php
@@ -58,7 +58,7 @@
             ServiceRegister::registerHttpClient($client);
 
           //  ServiceRegister::registerFilterStorage(new FilterStorage(null));
-            $filter = self::getPopulatedFilter($filterData, $filterType);
+          //  $filter = self::getPopulatedFilter($filterData, $filterType);
 
             // $filterRepository = new FilterRepository();
             // $filterResult = $filterRepository->saveFilter($pricemonitorId, $filter);
