b3e5f569240a1e5d1f628c29226b0f1ea499ebb3
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-07 11:10:35 +0200

    filterOriginal

--- resources/lib/FilterStorage.php
+++ resources/lib/FilterStorage.php
@@ -46,9 +46,10 @@ class FilterStorage implements FilterStorageInterface
       */
      public function getFilter($idContract, $typeFilter)
      {
-        //  $filterOriginal = $this->productRepo->getFilterByContractIdAndType($idContract,$typeFilter);
+         $filterOriginal = $this->productRepo->getFilterByContractIdAndType($idContract,$typeFilter);
 
-        //  return ($filterOriginal !== null) ? $filterOriginal['serializedFilter'] : null;
+         return $filterOriginal;
+         //  return ($filterOriginal !== null) ? $filterOriginal['serializedFilter'] : null;
      }
 }
 
