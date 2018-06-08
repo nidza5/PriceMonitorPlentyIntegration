eb919dac8afb4a45250c4b59c3c0b4dc62fb0b6b
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-08 08:33:46 +0200

    filter populated in main function

--- resources/lib/PriceMonitorSdkHelper.php
+++ resources/lib/PriceMonitorSdkHelper.php
@@ -60,7 +60,34 @@
             ServiceRegister::registerHttpClient($client);
 
            ServiceRegister::registerFilterStorage(new FilterStorage($productFilterRepo));
-           $filter = self::getPopulatedFilter($filterData, $filterType);
+          // $filter = self::getPopulatedFilter($filterData, $filterType);
+
+          $filterGroups = array();
+         foreach ($filterData as $key => $filterGroup) {
+            if (empty($filterGroup['expressions'])) {
+                continue;
+            }
+
+            $name = isset($filterGroup['name']) ? $filterGroup['name'] : ('Group ' . (++$key));
+            $group = new Group($name, $filterGroup['groupOperator']);
+
+            $expressions = array();
+            foreach ($filterGroup['expressions'] as $expression) {
+                $expressions[] = new Expression(
+                    $expression['code'],
+                    $expression['condition'],
+                    $expression['type'],
+                    $expression['value'],
+                    $filterGroup['operator']
+                );
+            }
+
+            $group->setExpressions($expressions);
+            $filterGroups[] = $group;
+        }
+
+        $filter = new Filter('Filter', $filterType);
+        $filter->setExpressions($filterGroups);
 
             $filterRepository = new FilterRepository();
             $filterResult = $filterRepository->saveFilter($pricemonitorId, $filter);
