8fd7710362e7473ad313413a443da78044de71d6
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-08 09:13:44 +0200

    json encode and decode

--- resources/lib/PriceMonitorSdkHelper.php
+++ resources/lib/PriceMonitorSdkHelper.php
@@ -65,7 +65,7 @@
             $filterRepository = new FilterRepository();
             $filterResult = $filterRepository->saveFilter($pricemonitorId, $filter);
             
-            return json_encode($filter);
+            return json_decode(json_encode($filter),TRUE);
 
         } catch(\Exception $ex) 
         {
