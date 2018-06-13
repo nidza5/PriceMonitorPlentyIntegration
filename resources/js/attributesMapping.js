d58ab2b745dd69e7cde6b83823f47c195b3b7d31
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-13 11:45:25 +0200

    quotation marks

--- resources/js/attributesMapping.js
+++ resources/js/attributesMapping.js
@@ -255,7 +255,7 @@ function setSavedValuesOnView(response) {
                 '<input type="text" name="customTagPricemonitorCode" class="pricemonitor-form-field">' +
                     '<div class="filterable-dropdown-wrapper">' +
                         '<select ' +
-                        'onchange=updateCustomTagValuesOnInput(this)' +
+                        'onchange="updateCustomTagValuesOnInput(this)" ' +
                         'class="pricemonitor-filterable-dropdown form-control pricemonitor-form-field pricemonitor-filterable-list attributes-mapping-custom-attributes" ' +
                         'name="customTagAttributeValue" ' +
                         'id="custom-tag-add" ' +
