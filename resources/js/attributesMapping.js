d60aa77ab1e7d495fa711083723adf193302f186
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-13 12:28:44 +0200

    bootstrap buttons

--- resources/js/attributesMapping.js
+++ resources/js/attributesMapping.js
@@ -267,7 +267,7 @@ function setSavedValuesOnView(response) {
                              'class="pricemonitor-form-field" ' +
                             'id="attribute-custom-tag-add"/>' +
                        '</div>' +
-                       '<button class="add-custom-tag" id="add-custom-tag">' +
+                       '<button class="add-custom-tag btn btn-success" id="add-custom-tag">' +
                         '+' +
                         '</button>';
 
@@ -371,7 +371,7 @@ function setSavedValuesOnView(response) {
                 'class="pricemonitor-form-field" ' +
                 'id="attribute-custom-tag-mapped-code-' + index + '" required/>' +
                 '</div>' +
-                '<button class="remove-custom-tag" id="remove-custom-tag-' + index + '">-</button>';
+                '<button class="remove-custom-tag btn btn-success" id="remove-custom-tag-' + index + '">-</button>';
 
             document.getElementById('attributes-mapping-custom-tags').appendChild(formRow);
 
