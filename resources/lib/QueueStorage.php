b126fe26cd76089a7c51a9f5494364d70153ee83
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-18 01:57:42 +0200

    return null insted false

--- resources/lib/QueueStorage.php
+++ resources/lib/QueueStorage.php
@@ -92,7 +92,7 @@ class QueueStorage implements Storage
         $queueItem = $this->queueModel;
 
         if ($queueItem == null) {
-            return false;
+            return null;
         }
 
         $reservationTime = $queueItem["reservationTime"] === null ? "" :
