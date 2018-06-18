749e3906610435ccd4c1ce8ff249e6963ff9e76b
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-18 09:36:47 +0200

    serialize storage model

--- resources/lib/QueueStorage.php
+++ resources/lib/QueueStorage.php
@@ -123,7 +123,7 @@ class QueueStorage implements Storage
        // within lib there is no way to do insert in database
 
        return ['queueName' => $queueName,
-               'storageModel' => $storageModel];
+               'storageModel' => serialize($storageModel)];
     }
 
     /**
