fa3eab4727b50197877611c5495a0a0562fe4728
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-19 23:50:02 +0200

    cccc

--- src/Services/ProductFilterService.php
+++ src/Services/ProductFilterService.php
@@ -83,7 +83,7 @@ class ProductFilterService {
             'variationRetailPrice' => [
                 'price'
             ],
-            'ItemProperty' => [
+            'itemProperty' => [
                 'propertyId'
             ]
         ];
