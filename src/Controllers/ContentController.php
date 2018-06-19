ccd770fb83ad446a1f72e188349e647a3d85f687
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-19 09:46:28 +0200

    no push values

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -161,9 +161,6 @@ namespace PriceMonitorPlentyIntegration\Controllers;
         $this->config->set('email',$credentials['email']);
         $this->config->set('password',$credentials['password']);
 
-        $this->config->push('email',$credentials['email']);
-        $this->config->push('password',$credentials['password']);
-
          $this->sdkService->call("setUpPriceMonitorCredentials", [
             'email' => $credentials['email'],
             'password' => $credentials['password']
