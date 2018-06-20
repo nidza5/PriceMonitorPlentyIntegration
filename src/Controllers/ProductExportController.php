eabb8c7fc26751d782873c092592c22d977f1034
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-20 12:48:17 +0200

    enqueu and run

--- src/Controllers/ProductExportController.php
+++ src/Controllers/ProductExportController.php
@@ -161,6 +161,8 @@ namespace PriceMonitorPlentyIntegration\Controllers;
             'passwordForConfig' =>  $passwordForConfig        
         ]); 
 
+        echo json_encode($enqueAndRun);
+
         if($enqueAndRun != null && $enqueAndRun['Message'])
         {
             return [
