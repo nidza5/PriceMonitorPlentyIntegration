53f18cc93ac0f3bd7b3fcdceabf1f1cf4f391d06
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-21 09:17:14 +0200

    work work

--- resources/lib/PriceMonitorSdkHelper.php
+++ resources/lib/PriceMonitorSdkHelper.php
@@ -196,18 +196,18 @@
                 $nameColumnInVariation = null;
                 $filteredProducts = array_filter($variationArray, function($value) use ($nameColumnInVariation,$condition,$values) {
                     if($condition == "equal") {
-                        return $value['name'] == $values[0];
+                        return $value['number'] == $values[0];
                     } else if($condition == "not_equal") {
                         return $value[$nameColumnInVariation] != $values[0];
                     }
                     else if($condition == "greater_than") {
-                        return $value['name'] > $values[0];
+                        return $value['number'] > $values[0];
                     } else if($condition == "less_than") {
-                        return $value['name'] < $values[0];
+                        return $value['number'] < $values[0];
                     } else if($condition == 'greater_or_equal') {
-                        return $value['name'] >= $values[0];
+                        return $value['number'] >= $values[0];
                     } else if($condition == 'less_or_equal') {
-                        return $value['name'] <= $values[0];
+                        return $value['number'] <= $values[0];
                     }                    
                 });
     
