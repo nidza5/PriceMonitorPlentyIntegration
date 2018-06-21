0c2be350d7e8bdb8b9c267fca4cbfd1eacb7abfc
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-21 17:41:02 +0200

    no include productService

--- resources/lib/PriceMonitorSdkHelper.php
+++ resources/lib/PriceMonitorSdkHelper.php
@@ -2,7 +2,7 @@
 
  require_once __DIR__ . '/PriceMonitorHttpClient.php';
  require_once __DIR__ . '/FilterStorage.php';
- require_once __DIR__ . '/ProductService.php';
+//  require_once __DIR__ . '/ProductService.php';
  require_once __DIR__ . '/TransactionStorage.php';
  require_once __DIR__ . '/ConfigService.php';
  require_once __DIR__ . '/MapperService.php';
