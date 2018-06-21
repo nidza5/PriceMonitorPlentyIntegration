563eb258c094d0b79e336a17809c6fc346ac8328
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-21 16:52:54 +0200

    no constructor in productService

--- resources/lib/ProductServices.php
+++ resources/lib/ProductServices.php
@@ -10,15 +10,16 @@ class ProductServices implements ProductService
    private $contract;
    private $productForExport;
 
-    public function __construct($contract,$productForExport)
-    {
-        $this->contract = $contract;
-        $this->productForExport = $productForExport;
-    }
+    // public function __construct($contract,$productForExport)
+    // {
+    //     $this->contract = $contract;
+    //     $this->productForExport = $productForExport;
+    // }
 
     public function exportProducts($contractId, $filter, array $shopCodes = array())
     {
-        return $this->productForExport;
+         return null;
+        //return $this->productForExport;
     }
 
     /**
