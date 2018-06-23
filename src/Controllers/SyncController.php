de4dca7aa2a5f4b6408d3e23db09bd45002af1df
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-23 15:38:06 +0200

    again.again

--- src/Controllers/SyncController.php
+++ src/Controllers/SyncController.php
@@ -178,9 +178,9 @@ namespace PriceMonitorPlentyIntegration\Controllers;
        
        echo json_encode($filteredVariation);
 
-       foreach ($filteredVariation as $shopProduct) {
-            echo json_encode($shopProduct);
-    }
+    //    foreach ($filteredVariation as $shopProduct) {
+    //         echo json_encode($shopProduct);
+    // }
 
         // $emailObject = $this->configInfoRepo->getConfig('email');
         // $passwordObject = $this->configInfoRepo->getConfig('password');
