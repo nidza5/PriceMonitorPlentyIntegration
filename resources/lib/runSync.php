88d6dc4207cd00a2f7f4e21af9fc44b1ab1e57db
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-24 11:17:03 +0200

    enqueue product export

--- resources/lib/runSync.php
+++ resources/lib/runSync.php
@@ -13,31 +13,29 @@ try {
 
    $filterType = SdkRestApi::getParam('filterType');
    $priceMonitorId = SdkRestApi::getParam('priceMonitorId');
+   $productFilterRepo = SdkRestApi::getParam('productFilterRepo');
 
-    return $priceMonitorId;
-//    $productFilterRepo = SdkRestApi::getParam('productFilterRepo');
+   $products = SdkRestApi::getParam('products');
 
-//    $products = SdkRestApi::getParam('products');
+   $productsAttributes = SdkRestApi::getParam('attributesFromPlenty');
+   $attributeMapping = SdkRestApi::getParam('attributeMapping');
+   $contract = SdkRestApi::getParam('contract');  
 
-//    $productsAttributes = SdkRestApi::getParam('attributesFromPlenty');
-//    $attributeMapping = SdkRestApi::getParam('attributeMapping');
-//    $contract = SdkRestApi::getParam('contract');  
-
-//    $savedInitialTransaction = SdkRestApi::getParam('savedTransactionMasterRecord');    
+   $savedInitialTransaction = SdkRestApi::getParam('savedTransactionMasterRecord');    
   
-//    PriceMonitorSdkHelper::registerConfigService($emailForConfig,$passwordForConfig);
+   PriceMonitorSdkHelper::registerConfigService($emailForConfig,$passwordForConfig);
 
-//    PriceMonitorSdkHelper::registerMapperService($attributeMapping,$contract,$products,$productsAttributes); 
-//    PriceMonitorSdkHelper::registerProductService($contract,$products); 
+   PriceMonitorSdkHelper::registerMapperService($attributeMapping,$contract,$products,$productsAttributes); 
+   PriceMonitorSdkHelper::registerProductService($contract,$products); 
 
-//     PriceMonitorSdkHelper::registerTransactionHistotyStorage(null,0,null,0,$savedInitialTransaction);
+    PriceMonitorSdkHelper::registerTransactionHistotyStorage(null,0,null,0,$savedInitialTransaction);
 
-//    $savedMasterTransactionHistory = SdkRestApi::getParam('savedTransactionMasterRecord');
+   $savedMasterTransactionHistory = SdkRestApi::getParam('savedTransactionMasterRecord');
    
 
-//    $runnerService = new RunnerService($queueModel);
-//    $runnerService->enqueueProductExportJob($priceMonitorId);
-//    return  $runnerService->runSync($queueName,$products,$savedInitialTransaction['id'], $priceMonitorId);
+   $runnerService = new RunnerService($queueModel);
+   $runnerService->enqueueProductExportJob($priceMonitorId);
+   return  $runnerService->runSync($queueName,$products,$savedInitialTransaction['id'], $priceMonitorId);
 
 } catch(\Exception $ex) {
     
