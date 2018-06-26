759e2f52f9daa073c2b8c171b02b82b5d47f7cc2
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-26 09:17:34 +0200

    dsdasdasd

--- src/Services/ProductFilterService.php
+++ src/Services/ProductFilterService.php
@@ -107,7 +107,7 @@ class ProductFilterService {
             'with' => [
                 'variationAttributeValues' => null,
                 'variationBarcodes' => 'barcode',
-                'item' => null,
+                'item' => 'itemProperties',
                 'variationCategories' => null,
                 'variationSuppliers'  => null
             ]
