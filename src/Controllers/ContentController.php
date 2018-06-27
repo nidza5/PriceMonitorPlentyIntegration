29e9a47a51de92cb976985c3dbc3469e5ff67741
Nikola Vasiljević <vnidza91@gmail.com>
2018-06-27 13:31:02 +0200

    show all variations

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -238,8 +238,8 @@ namespace PriceMonitorPlentyIntegration\Controllers;
 
         $itemService = pluginApp(ProductFilterService::class);
 
-       // $finalResult = $itemService->getAllVariations();
-        $finalResult = $itemService->getManufacturerById(2);
+        $finalResult = $itemService->getAllVariations();
+       // $finalResult = $itemService->getManufacturerById(2);
         // $finalResult = $itemService->getItemWithPropertiesById(135);
        
 
