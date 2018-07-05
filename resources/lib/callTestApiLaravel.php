5d5fcd07a8a94783c3833c121157faba8bdd419e
Nikola Vasiljević <vnidza91@gmail.com>
2018-07-05 18:22:02 +0200

    try

--- resources/lib/callTestApiLaravel.php
+++ resources/lib/callTestApiLaravel.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/PriceMonitorHttpClient.php';
 $client = new PriceMonitorHttpClient();
 $res = $client->request(
     'GET',
-    '127.0.0.1:8012/articles'
+    'https://jsonplaceholder.typicode.com/posts/1'
 );
  
 /** @return array */
