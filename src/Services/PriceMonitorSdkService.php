1cfb3cee514874a954a28a915661250296391fcd
Nikola Vasiljević <vnidza91@gmail.com>
2018-07-10 18:00:53 +0200

    gateway url

--- src/Services/PriceMonitorSdkService.php
+++ src/Services/PriceMonitorSdkService.php
@@ -7,7 +7,7 @@ use Plenty\Plugin\ConfigRepository;
 class PriceMonitorSdkService
 {
 
-    const GATEWAY_BASE_PATH = 'http://1b4dd1fa.ngrok.io';
+    const GATEWAY_BASE_PATH = 'http://bb78c34e.ngrok.io';
 
     /**
      *
