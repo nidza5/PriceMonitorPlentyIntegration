09a0f53fb3c7c13d6f6d890c5bd618c84574d338
Nikola Vasiljević <vnidza91@gmail.com>
2018-07-11 08:40:05 +0200

    ngrok new url

--- src/Services/PriceMonitorSdkService.php
+++ src/Services/PriceMonitorSdkService.php
@@ -7,7 +7,7 @@ use Plenty\Plugin\ConfigRepository;
 class PriceMonitorSdkService
 {
 
-    const GATEWAY_BASE_PATH = 'http://bb78c34e.ngrok.io';
+    const GATEWAY_BASE_PATH = 'http://e042c519.ngrok.io';
 
     /**
      *
