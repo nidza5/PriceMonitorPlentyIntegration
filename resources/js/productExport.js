2ae2505fd039d6a4b27ed8218e2a92b4ea0bffd4
Nikola Vasiljević <vnidza91@gmail.com>
2018-07-17 13:53:59 +0200

    no sync

--- resources/js/productExport.js
+++ resources/js/productExport.js
@@ -72,13 +72,13 @@
             $('#lastExportStartedAt').html(contract.exportStart);
             $('#statusLastExport').html(contract.exportStatus);
             $('#successfullyLastExport').html(contract.exportSuccessCount);
-            $(".pricemonitor-no-sync-data").hide();
+            $("#productExportNoSync").hide();
             $("#dataLastExport").show();           
         }
         else
         {
             $("#dataLastExport").hide();
-            $(".pricemonitor-no-sync-data").show();
+            $("#productExportNoSync").show();
         }
     }
 
