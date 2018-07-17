9b61fe6db37a5fe81151e35ddd114f5cbed0f195
Nikola Vasiljević <vnidza91@gmail.com>
2018-07-17 12:38:19 +0200

    no echo in transaction

--- src/Controllers/TransactionHistoryController.php
+++ src/Controllers/TransactionHistoryController.php
@@ -111,16 +111,10 @@ namespace PriceMonitorPlentyIntegration\Controllers;
         $pricemonitorId = $requestData['pricemonitorId'];
         $type = $requestData['type'];
 
-        echo "last transaction history";
-       
-
         $transaction =  $this->sdkService->call("getLastTransactionHistoryFromMiddleware", [
             'pricemonitorId' => $pricemonitorId,
             'type' => $type         
         ]);
-
-        echo "transaction";
-        echo json_encode($transaction );
 
         return json_encode($transaction); 
     }
