96c292287a38c2e0c10a7dab6bb8a987ab64d954
Nikola Vasiljević <vnidza91@gmail.com>
2018-08-06 15:49:11 +0200

    comment before

--- src/Middlewares/Middleware.php
+++ src/Middlewares/Middleware.php
@@ -24,21 +24,19 @@ class Middleware extends \Plenty\Plugin\Middleware
     public function before(Request $request )
     {
 
-        return $this->response->make("Unauthorized request", 401);
+        // $jwt = $request->header('Authorization', null);
+        // $access_token = "";
 
-        $jwt = $request->header('Authorization', null);
-        $access_token = "";
+        // if ($jwt != null ) {
+        //     $access_token = explode(" ",$jwt)[1];
+        // }
 
-        if ($jwt != null ) {
-            $access_token = explode(" ",$jwt)[1];
-        }
+        // $savedToken =  $this->configInfoRepo->getConfig('access_token');
+        // $token = $savedToken !== null ? $savedToken->value : "";
 
-        $savedToken =  $this->configInfoRepo->getConfig('access_token');
-        $token = $savedToken !== null ? $savedToken->value : "";
-
-        if ($access_token === null || $access_token != $token) {
-            return $this->response->make("Unauthorized request", 401);
-        }
+        // if ($access_token === null || $access_token != $token) {
+        //     return $this->response->make("Unauthorized request", 401);
+        // }
     }
 
 }
