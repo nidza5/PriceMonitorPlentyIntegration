f0e366c25b1868cd9ae82f2e1ce07b00e92bc2b1
Nikola Vasiljević <vnidza91@gmail.com>
2018-08-06 23:54:17 +0200

    parametar check token

--- src/Middlewares/Middleware.php
+++ src/Middlewares/Middleware.php
@@ -23,20 +23,24 @@ class Middleware extends \Plenty\Plugin\Middleware
 
     public function before(Request $request )
     {
+        $checkToken = $request->get('isCheck', null);
 
-        // $jwt = $request->header('Authorization', null);
-        // $access_token = "";
+        if ($checkToken != null && $checkToken == true)
+        {
+            $jwt = $request->header('Authorization', null);
+            $access_token = "";
 
-        // if ($jwt != null ) {
-        //     $access_token = explode(" ",$jwt)[1];
-        // }
+            if ($jwt != null ) {
+                $access_token = explode(" ",$jwt)[1];
+            }
 
-        // $savedToken =  $this->configInfoRepo->getConfig('access_token');
-        // $token = $savedToken !== null ? $savedToken->value : "";
+            $savedToken =  $this->configInfoRepo->getConfig('access_token');
+            $token = $savedToken !== null ? $savedToken->value : "";
 
-        // if ($access_token === null || $access_token != $token) {
-        //     return $this->response->make("Unauthorized request", 401);
-        // }
+            if ($access_token === null || $access_token != $token) {
+                return $this->response->make("Unauthorized request", 401);
+            }
+        }        
     }
 
     public function after(Request $request, Response $response):Response
