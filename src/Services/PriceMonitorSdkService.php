a236fa68f590006beb21234bbdf4650fcf547004
Nikola Vasiljević <vnidza91@gmail.com>
2018-08-08 18:16:34 +0200

    ngrok on different port

--- src/Services/PriceMonitorSdkService.php
+++ src/Services/PriceMonitorSdkService.php
@@ -11,7 +11,7 @@ use PriceMonitorPlentyIntegration\Repositories\ConfigInfoRepository;
 class PriceMonitorSdkService
 {
 
-    const GATEWAY_BASE_PATH = 'http://e797bd29.ngrok.io';
+    const GATEWAY_BASE_PATH = 'http://add72f92.ngrok.io';
 
     /**
      *
